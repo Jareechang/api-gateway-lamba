462caa444cdd5967edb1f8e1ffc44967c5277a99
Jerry Chang <therealjerrycan@gmail.com>
2020-08-15 11:59:01 -0700

    Refactor crud lambda functions folder structure

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "list_pets" {
     function_name = "${var.lambda_name}-${var.env}-list-pets"
     s3_bucket = "${aws_s3_bucket.lambda.id}"
     s3_key = "${aws_s3_bucket_object.lambda.id}"
-    handler = "src/pet-list/index.handler"
+    handler = "src/pet/list.handler"
     role = "${aws_iam_role.lambda_role.arn}"
     timeout = var.lambda_timeout 
     source_code_hash = "${filebase64sha256("dist/${aws_s3_bucket_object.lambda.id}.zip")}"
@@ -44,7 +44,7 @@ resource "aws_lambda_function" "get_pet" {
     function_name = "${var.lambda_name}-${var.env}-get-pet"
     s3_bucket = "${aws_s3_bucket.lambda.id}"
     s3_key = "${aws_s3_bucket_object.lambda.id}"
-    handler = "src/pet/index.handler"
+    handler = "src/pet/get.handler"
     role = "${aws_iam_role.lambda_role.arn}"
     timeout = var.lambda_timeout 
     source_code_hash = "${filebase64sha256("dist/${aws_s3_bucket_object.lambda.id}.zip")}"
