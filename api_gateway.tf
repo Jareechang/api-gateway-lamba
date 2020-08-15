c0146aedfc021e05abe5ffc29fd341c788bb01b8
Jerry Chang <therealjerrycan@gmail.com>
2020-08-14 22:09:17 -0700

    Update to create pets crud

--- api_gateway.tf
+++ api_gateway.tf
@@ -5,26 +5,21 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "api_resource" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-    path_part   = "{proxy+}"
+    path_part   = "pets"
 }
 
-resource "aws_api_gateway_method" "get_method" {
+resource "aws_api_gateway_method" "list" {
     rest_api_id   = aws_api_gateway_rest_api.api.id
     resource_id   = aws_api_gateway_resource.api_resource.id
-    http_method   = "ANY"
+    http_method   = "GET"
     authorization = "NONE"
-
-    request_parameters = {
-        "method.request.querystring.accountId" = true
-        "method.request.querystring.token" = true
-    }
 }
 
 
 resource "aws_api_gateway_integration" "integration" {
     rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = aws_api_gateway_resource.api_resource.id
-    http_method             = aws_api_gateway_method.get_method.http_method
+    http_method             = aws_api_gateway_method.list.http_method
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
     uri                     = aws_lambda_function.api_gateway_lambda.invoke_arn
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 exports.handler = async function (event, context, callback) {
+    const pets = [
+        { 
+            "id": 1, 
+            "type": "dog", 
+            "price": 249.99 
+        }, 
+        { 
+            "id": 2, 
+            "type": "cat", 
+            "price": 124.99 
+        }, 
+        { 
+            "id": 3, 
+            "type": "fish", 
+            "price": 0.99 
+        } 
+    ];
     const queryString = event["queryStringParameters"];
 
     console.log('QueryString: ', JSON.stringify(queryString));
+
     var response = {
         statusCode: 200,
         headers: {
-            'Content-Type': 'text/html; charset=utf-8',
+            'Content-Type': 'application/json;',
         },
-        body: '<p>Hello world!</p>',
+        body: pets,
     }
     callback(null, response)
 }
